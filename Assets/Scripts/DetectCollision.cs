6dc1d4f4d76e3243f88d05bc30ce8cba98158b37
1Ricardo5 <114586483+1Ricardo5@users.noreply.github.com>
2023-10-31 12:21:52 -0400

    Initial commit

---
+++ Assets/Scripts/DetectCollision.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+
+    public int scoreToAdd;
+
+    private void OnTriggerEnter(Collider other)
+    {
+  
+        if(other.CompareTag("Player"))
+        {
+            PlayerController pc = other.GetComponent<PlayerController>();
+            pc.score += scoreToAdd;
+            Debug.Log(pc.score);
+            Destroy(gameObject);
+        }else
+        {
+            Destroy(gameObject);
+        }
+        
+    }  
+}   
+
